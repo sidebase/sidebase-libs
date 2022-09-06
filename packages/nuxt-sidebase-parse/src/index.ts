27187f1213d6323d9782fa3cbdc0f83c7d9d5b64
BracketJohn <njonalik@protonmail.com>
2022-09-06 17:01:21 +0200

    feat(nuxt-sidebase-parse): switch to functions

--- packages/nuxt-sidebase-parse/src/index.ts
+++ packages/nuxt-sidebase-parse/src/index.ts
@@ -21,81 +21,79 @@ const apiValidateWithSchema = <ZodSchema extends z.ZodTypeAny>(
 }
 
 /**
- * Make a data parser that takes in an object `O` from which to extract data and a zod-data-schema. It then extracts data `D` from `O` to then parse and validate the data `D` using the zod schema.
- * This method will throw HTTP Exceptions (like 422 Unprocessible Entity) if data validation fails. The HTTP Error code and message can be customized.
- * @param {DataFetchingFunction} dataFetchingFunction - that accepts one argument (the `O`) and returns some data `D` from it
- * @param {number} errorCode - HTTP status code to return to client if parsing and validating `D` fails
- * @param {string} errorMessage - message to return to client if parsing and validating `D` fails
- */
-type DataFetchingFunction<T> = (param: T) => any
-const makeParserForDataSource = <T>(dataFetchingFunction: DataFetchingFunction<T>) => {
-  const parser = async <ZodSchema extends z.ZodTypeAny>(input: Parameters<typeof dataFetchingFunction>[0], schema: ZodSchema, errorCode = 422, errorMessage = "Data validation failed") => {
-    const data = await dataFetchingFunction(input)
-    return apiValidateWithSchema(data, schema, errorCode, errorMessage)
-  }
-
-  return parser
-}
-
-/**
  * Parse the body of a `h3` event.
  *
  * Cookies are part of the HTTP standard and may be send with a request.
  *
- * @param {CompatibilityEvent} input - Input to parser using the `schema`
+ * @param {CompatibilityEvent} event - Input to parser using the `schema`
  * @param {ZodSchema} schema - Error code of error if parsing fails
  * @param {string} [errorCode=422] - Optional error message if parsing fails
  * @param {string} [errorMessage="Data validation failed"] - Optional error message if parsing fails
  */
-const parseBodyAs = makeParserForDataSource(readBody<any>)
+async function parseBodyAs<ZodSchema extends z.ZodTypeAny>(event: CompatibilityEvent, schema: ZodSchema, errorCode = 422, errorMessage = "Body validation failed") {
+  const data = await readBody(event)
+  return apiValidateWithSchema(data, schema, errorCode, errorMessage)
+}
 
 /**
  * Parse the params of a `h3` event.
  *
  * For example `/[test].get.ts` binds the parameter `test` to a value, for example `/1` then results in `test = 1`
  *
- * @param {CompatibilityEvent} input - Input to parser using the `schema`
+ * @param {CompatibilityEvent} event - Input to parser using the `schema`
  * @param {ZodSchema} schema - Error code of error if parsing fails
  * @param {string} [errorCode=422] - Optional error message if parsing fails
  * @param {string} [errorMessage="Data validation failed"] - Optional error message if parsing fails
  */
-const parseParamsAs = makeParserForDataSource((event: CompatibilityEvent) => event.context.params)
+function parseParamsAs<ZodSchema extends z.ZodTypeAny>(event: CompatibilityEvent, schema: ZodSchema, errorCode = 422, errorMessage = "Query validation failed") {
+  const data = event.context.params
+  return apiValidateWithSchema(data, schema, errorCode, errorMessage)
+}
 
 /**
  * Parse the query of a `h3` event.
  *
  * For example `/bar?sort=ASC` binds the query value `sort = "ASC"`
  *
- * @param {CompatibilityEvent} input - Input to parser using the `schema`
+ * @param {CompatibilityEvent} event - Input to parser using the `schema`
  * @param {ZodSchema} schema - Error code of error if parsing fails
  * @param {string} [errorCode=422] - Optional error message if parsing fails
  * @param {string} [errorMessage="Data validation failed"] - Optional error message if parsing fails
  */
-const parseQueryAs = makeParserForDataSource(getQuery)
+function parseQueryAs<ZodSchema extends z.ZodTypeAny>(event: CompatibilityEvent, schema: ZodSchema, errorCode = 422, errorMessage = "Query validation failed") {
+  const data = getQuery(event)
+  return apiValidateWithSchema(data, schema, errorCode, errorMessage)
+}
 
 /**
  * Parse the cookies of a `h3` event.
  *
  * Cookies are part of the HTTP standard and send with every request.
  *
- * @param {CompatibilityEvent} input - Input to parser using the `schema`
+ * @param {CompatibilityEvent} event - Input to parser using the `schema`
  * @param {ZodSchema} schema - Error code of error if parsing fails
  * @param {string} [errorCode=422] - Optional error message if parsing fails
  * @param {string} [errorMessage="Data validation failed"] - Optional error message if parsing fails
  */
-const parseCookieAs = makeParserForDataSource(parseCookies)
+function parseCookieAs<ZodSchema extends z.ZodTypeAny>(event: CompatibilityEvent, schema: ZodSchema, errorCode = 422, errorMessage = "Query validation failed") {
+  const data = parseCookies(event)
+  return apiValidateWithSchema(data, schema, errorCode, errorMessage)
+}
 
 /**
  * Parse the headers of a `h3` event.
  *
  * Cookies are part of the HTTP standard and send with every request.
  *
- * @param {CompatibilityEvent} input - Input to parser using the `schema`
+ * @param {CompatibilityEvent} event - Input to parser using the `schema`
  * @param {ZodSchema} schema - Error code of error if parsing fails
  * @param {string} [errorCode=422] - Optional error message if parsing fails
  * @param {string} [errorMessage="Data validation failed"] - Optional error message if parsing fails
  */
-const parseHeaderAs = makeParserForDataSource(getHeaders)
+function parseHeaderAs<ZodSchema extends z.ZodTypeAny>(event: CompatibilityEvent, schema: ZodSchema, errorCode = 422, errorMessage = "Query validation failed") {
+  const data = getHeaders(event)
+  return apiValidateWithSchema(data, schema, errorCode, errorMessage)
+}
 
 /**
  * Parse arbitrary data using a schema.
@@ -117,12 +115,15 @@ const parseHeaderAs = makeParserForDataSource(getHeaders)
  * // -> output: `1` (as a number, as `z` also deserializes)
  * ```
  *
- * @param {CompatibilityEvent} input - Input to parser using the `schema`
+ * @param {any | Promise<any>} dataOrPromise - Input to parser using the `schema`
  * @param {ZodSchema} schema - Error code of error if parsing fails
  * @param {string} [errorCode=422] - Optional error message if parsing fails
  * @param {string} [errorMessage="Data validation failed"] - Optional error message if parsing fails
  */
-const parseDataAs = makeParserForDataSource(async (data: Promise<any>) => await data)
+async function parseDataAs<ZodSchema extends z.ZodTypeAny>(dataOrPromise: any | Promise<any>, schema: ZodSchema, errorCode = 422, errorMessage = "Data validation failed") {
+  const data = await dataOrPromise
+  return apiValidateWithSchema(data, schema, errorCode, errorMessage)
+}
 
 /**
  * Make a data transformer based on a schema. All data passed into it will be turned into data of that schema (or the transformer will throw during parsing)
@@ -146,8 +147,9 @@ const parseDataAs = makeParserForDataSource(async (data: Promise<any>) => await 
  * // -> output: `object` (this is a parsed date, not a date string!)
  * ```
  */
-const makeParser = <ZodSchema extends z.ZodTypeAny>(schema: ZodSchema, errorCode = 422, errorMessage = "Data validation failed") => (data: any, errorCodeOverwrite = undefined, errorMessageOverwrite = undefined) => apiValidateWithSchema(data, schema, errorCodeOverwrite || errorCode, errorMessageOverwrite || errorMessage)
-
+function makeParser<ZodSchema extends z.ZodTypeAny>(schema: ZodSchema, errorCode = 422, errorMessage = "Data validation failed") {
+  return async (data: any | Promise<any>, errorCodeOverwrite = undefined, errorMessageOverwrite = undefined) => apiValidateWithSchema(await data, schema, errorCodeOverwrite || errorCode, errorMessageOverwrite || errorMessage)
+}
 
 export {
   parseBodyAs,
